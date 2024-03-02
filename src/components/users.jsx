7e0821cb18c2e99638f28377f57bf48b74247b74
Honeynem <148876783+Honeynem@users.noreply.github.com>
2024-03-02 12:23:27 +0330

    recieving and processing data from an api

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -6,23 +6,49 @@ class Users extends React.Component{
         users : []
     }
 
+    async componentDidMount(){
+        const response =  await axios.get('https://reqres.in/api/users')
+        console.log(response)
+        this.setState({users : response.data.data})
+    }
 
     render(){
         return(
             <>
-            <button className="btn btn-lg btn-primary"></button>
+            <button onClick={this.handleCreate} className="btn btn-lg btn-primary">Create</button>
             <div className="row">
                 {this.state.users.map((user)=>{
                     return (
                         <div className="col-4 text-center p-5">
                             <img src={user.avatar} style={{borderRadius : '50%', width: '100px'}} />
                             <h4>{user.first_name} {user.last_name}</h4>
+                            <h5>{user.email}</h5>
+                            <div className="row">
+                                <div className="col-6">
+                                    <button onClick={this.handleUpdate} className='btn btn-info btn-sm'>Update</button>
+                                </div>
+                                <div className="col-6">
+                                    <button onClick={this.handleDelete} className='btn btn-danger btn-sm'>Update</button>
+                                </div>
+                            </div>
                         </div>
                     )
                 })}
             </div>
             </>
         )
+    }
+
+    handleCreate = ()=>{
+
+    }
+
+    handleUpdate = ()=>{
+
+    }
+
+    handleDelete = ()=>{
+
     }
 
 }
