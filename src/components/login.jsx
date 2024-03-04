19dacdbfe2f34720ee3c45aba9989810ec60c19f
Honeynem <148876783+Honeynem@users.noreply.github.com>
2024-03-04 15:06:38 +0330

    validation

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { Component } from 'react'
 import Input from './input'
+import * as yup from 'yup'
 
 class Login extends React.Component {
 
@@ -8,11 +9,39 @@ class Login extends React.Component {
         account: {
             email: '',
             password: ''
+        },
+        errors: []
+    }
+
+    validate = async()=>{
+        try{
+            const result = await this.schema.validate(this.state.account, {abortEarly: false})
+            return result;
+
+        } catch(error){
+            console.log(error.errors)
+            this.setState({})
+
         }
     }
+
+    schema = yup.object().shape({
+        email: yup.string().email('email format is not correct').required('email is required') ,
+        password: yup.string().min(4, 'password must contain at least 4 character')
+    })
     
     handleSubmit = async(e)=>{
         e.preventDefault()
+        const result = await this.validate()
+        if (result){
+            try{
+                const response = await axios.post('https://reqres.in/api/login', result)
+            console.log(response)
+            } catch(error){
+                this.setState({errors: ['email or password is not correct']})
+
+            }
+        }
     }
 
     handleChange = (e)=>{
@@ -24,19 +53,34 @@ class Login extends React.Component {
 
     render() {
         return (
+            
+            <>
+            {
+                this.state.errors.length !== 0 && (
+                    <div className="alert alert-danager">
+                    <ul>{
+                        this.state.errors.map((e, i)=> <li key={i}>{e}</li>)
+
+                    }
+                    </ul>
+                    </div>
+                )
+            }
             <form onSubmit={this.handleSubmit}>
                 {/* <div className="mb-3">
                     <label htmlFor="email">Email</label>
                     <input oncChange ={this.handleChange} name='email' value={this.state.account.email} id='email' type="text" className='form-control' />
                 </div> */}
-                <Input name='email' value='email' label='Email'onChange={this.handleChange} />
-                <Input name='password' value='password' label='Password' onChange={this.handleChange} />
                 {/* <div className="mb-3">
                     <label htmlFor="password">Password</label>
                     <input oncChange ={this.handleChange} name='password' value={this.state.account.password} id='password' type="text" className='form-control' />
                 </div> */}
+                <Input name='email' value='email' label='Email'onChange={this.handleChange} />
+                <Input name='password' value='password' label='Password' onChange={this.handleChange} />
+                
                 <button className="btn btn-primary">Login</button>
             </form>
+            </>
         )
     }
 }
