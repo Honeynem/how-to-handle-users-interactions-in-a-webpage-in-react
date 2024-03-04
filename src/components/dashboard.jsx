e7b7d1bcc80f969bb283bed51fb0b27424e2a90a
Honeynem <148876783+Honeynem@users.noreply.github.com>
2024-03-04 17:49:08 +0330

    query with token

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+
+
+//sending query with token to api in header
+axios.defaults.headers.common['token'] = localStorage.getItem('token')
 
 const Dashboard = () => {
+    useEffect(async()=>{
+        const response = await axios.get('https://reqres.in/api/unknown')
+        console.log(response)
+    }, [])
   return (
     <div>
       <h1>Dashboard</h1>
